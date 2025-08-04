0915bc47e277127663b302c6111a6dbfa0c8a061
arthurvignati <arthurvig8@gmail.com>
2025-08-04 16:35:09 -0300

    Starting studies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿namespace DotnetVisualStudioCode;
 
+
+
 public class Program
 {
-    public static void Main(){
+    public static void Main()
+    {
         Console.WriteLine("Hello World!");
+        string myName = "Kelvin";
+        int number = 20;
+        Console.WriteLine($"Nome: {myName} || Idade: {number}");
     }
 }
